efb1efc8b4f47fa831367a2a70fe31316b638e66
Lê Văn Hậu <levanhaum@gmail.com>
2026-06-04 13:55:47 +0700

    Setup Django project structure

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from categories.models import Category
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
